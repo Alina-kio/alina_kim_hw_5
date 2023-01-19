440fca917575a5599e5f3988087a670964d52f52
Alina-kio <alinaolkim@gmail.com>
2023-01-20 00:23:41 +0600

    hw on ingenuity

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,6 @@
 public class Hero {
     private int health;
-    String attackType;
+    private String attackType;
     private int damage;
 
     public int getHealth() {
@@ -10,14 +10,24 @@ public class Hero {
         return  damage;
     }
 
-    public Hero(int thehealth, String theattackType, int thedamage) {
-        health = thehealth;
-        attackType = theattackType;
-        damage = thedamage;
+    public String getAttackType() {
+        return attackType;
+    }
+
+    public Hero(int theHealth, String theAttackType, int theDamage) {
+        health = theHealth;
+        attackType = theAttackType;
+        damage = theDamage;
     }
     public Hero(int health, int damage){
         this.health =  health;
         this.damage = damage;
+    }
+
+    public Hero(int health, int damage, String attackType){
+        this.health =  health;
+        this.damage = damage;
+        this.attackType = attackType;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,31 @@
-import java.util.concurrent.locks.AbstractOwnableSynchronizer;
-
 public class Main {
     public static void main(String[] args) {
 
 
 
         Hero Berserk = new Hero(100, "magican", 35);
-        System.out.println("Berserk health: " + Berserk.getHealth() + " attackType: " + Berserk.attackType + " damage: " + Berserk.getDamage());
+        System.out.println("Berserk health: " + Berserk.getHealth() + " attackType: " + Berserk.getAttackType() + " damage: " + Berserk.getDamage());
 
         Hero Golem = new Hero( 100, 50);
         System.out.println("Golem health: " + Golem.getHealth() + " damage: " + Golem.getDamage());
 
         Boss Alina = new Boss(150, 700, "Critical Damage " );
-        System.out.println("Boss Damage: " + Alina.getBossDamage() + " Health: " + Alina.getBossHealth() + " Attack Type:" + Alina.getBossAttackType());
+        System.out.println("Boss Damage: " + Alina.getBossDamage() + " Health: " + Alina.getBossHealth() + " Attack Type: " + Alina.getBossAttackType());
+
+
+        System.out.println();
+        createHeroes();
+    }
+
+    public static Hero[] createHeroes(){
+        Hero hero1 = new Hero(120, "physical", 40);
+        Hero hero2 = new Hero(150, 50, "physical");
+
+        Hero[] hero = {hero1, hero2};
+        for (int i = 0; i < hero.length; i++) {
+            System.out.println("Health: " + hero[i].getHealth() + " Damage: " + hero[i].getDamage() + " Attack Type: " + hero[i].getAttackType());
+        }
+        return hero;
     }
 
 }
